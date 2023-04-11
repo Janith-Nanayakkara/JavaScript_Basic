91f14a644f07d5e4f8b02a98acb3d900b640d513
Janith-Nanayakkara <janithnanayakkara0202@gmail.com>
2023-04-11 16:52:47 +0530

    detect dark mdoe

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
+//random String
 const randomString = Math.random().toString(36).slice(2);
 console.log(randomString);
+//detect dark mode
+const isDarkMode = window.matchMedia && 
+window.matchMedia('(prefers-color-scheme:dark)').match;
